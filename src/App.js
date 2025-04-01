db660a3c17077353456d79db1a31303281c4c7e1
Cassia Ferraz <cassialeticia.n.f@outlook.com>
2025-04-01 14:46:36 -0300

    tema, criei card e estilo dele

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import {Estilos} from "./componentes/EstilosGlobais/Estilos";
+import Estilos from "./componentes/EstilosGlobais/Estilos";
+import Card from "./Card/Card";
+import ProvedorTema from "./componentes/ProvedorTema/ProvedorTema";
 
 function App() {
   return (
-    <div className="App">
+    <ProvedorTema>
       <Estilos/>
-      <h1>Freelando</h1>
-    </div>
+      <Card>
+        <h1>Freelando</h1>
+      </Card>
+    </ProvedorTema>
   );
 }
 
